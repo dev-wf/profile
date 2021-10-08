d1f561e9488d58763a9365834dc0c325ee7dab86
Welington Fernandes <w.a.f@outlook.com.br>
2021-10-08 00:39:14 -0300

    UP

--- src/js/database.js
+++ src/js/database.js
@@ -13,11 +13,42 @@ function submit() {
 }
 
 function swit() {
-    valor = document.getElementById('valor').value;
     //document.getElementById('setValor').innerText = getValor;
+
+    //valor:
+    valor = document.getElementById('valor').value;
     console.log(valor);
     document.getElementById('getValor').innerHTML = valor;
 
+    //pagador:
+    pagador = document.getElementById('pagador').value;
+    console.log(pagador);
+    document.getElementById('getPagador').innerHTML = pagador;
+
+    //cpfcnpj:
+    cpfcnpj = document.getElementById('cpfcnpj').value;
+    console.log(cpfcnpj);
+    document.getElementById('getDocPagador').innerHTML = cpfcnpj;
+
+    //referente:
+    referente = document.getElementById('referente').value;
+    console.log(referente);
+    document.getElementById('getRef').innerHTML = referente;
+
+    //emissor:
+    emissor = document.getElementById('emissor').value;
+    console.log(valor);
+    document.getElementById('getEmissor').innerHTML = emissor;
+
+    //cpfcnpj1:
+    cpfcnpj1 = document.getElementById('cpfcnpj1').value;
+    console.log(cpfcnpj1);
+    document.getElementById('getValorDocEmissor').innerHTML = cpfcnpj1;
+
+    //data:
+    data = document.getElementById('data').value;
+    console.log(data);
+    document.getElementById('getData').innerHTML = data;
 
 
 
